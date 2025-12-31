96436f6fc8a52bea84848cb3bdc0f1c0c7dd315a
gabzprogcompany <gabrielquiambao2929@gmail.com>
2025-12-31 14:16:18 +0800

    first commit

---
+++ exam_score_predict.session.sql
@@ -0,0 +1,120 @@
+SELECT *
+FROM
+    exams_predictions
+
+
+/*
+How many students are in each course?
+*/
+SELECT 
+    course,
+    COUNT(course) AS Student_Enrolled
+FROM
+    exams_predictions
+GROUP BY
+    course
+
+/*
+What percentage of students have internet access?
+*/
+SELECT
+    COUNT(student_id) as total_students,
+    SUM(CASE WHEN internet_access = 'TRUE' THEN 1 ELSE 0 END) as with_internet,
+    ROUND(AVG(CASE WHEN internet_access = 'TRUE' THEN 1 ELSE 0 END) * 100, 2) as percentage_with_internet
+FROM    
+    exams_predictions
+
+/*
+What's the average sleep hours for students who scored above 80?
+*/
+SELECT
+    exam_score,
+    AVG(sleep_hours) as Sleep
+FROM 
+    exams_predictions
+GROUP BY 
+    exam_score 
+HAVING 
+    exam_score >= 80
+ORDER BY
+    sleep 
+
+/*
+Does class attendance correlate with exam scores?
+*/
+SELECT
+    CASE
+        WHEN class_attendance >= 80 THEN 'High Attendance Rate'
+        WHEN class_attendance BETWEEN 50 AND 79 THEN 'Medium Attendance Rate'
+        ELSE 'Low Attendance Rate'
+    END AS Attendance_Rate,
+
+    COUNT(student_id) AS num_studs,
+    ROUND(AVG(exam_score),2) AS avg_scores
+
+FROM
+    exams_predictions
+GROUP BY 
+    Attendance_Rate
+
+/*
+How many percent of student who have high grades and their quality of sleep
+*/
+SELECT 
+    sleep_quality,
+    COUNT(student_id) AS total_students,
+    COUNT(CASE WHEN exam_score >= 90 THEN 1 END) AS high_grade_students,
+    ROUND(COUNT(CASE WHEN exam_score >= 90 THEN 1 END ) * 100 / COUNT(student_id), 2) AS student_highgrades
+FROM
+    exams_predictions
+GROUP BY
+    sleep_quality
+HAVING
+    sleep_quality = 'good'
+
+/*
+What's the relationship between study hours and exam scores? (Group students into study hour brackets: 0-5, 6-10, 11-15, 16+)
+*/
+SELECT
+    CASE
+        WHEN sleep_hours IS NULL THEN 'No Recorded Data'
+        WHEN sleep_hours < 5 THEN 'Low Sleep'
+        WHEN sleep_hours BETWEEN 5 AND 6.9 THEN 'Medium Rest'
+        ELSE 'Full Sleep'
+    END AS Levels_Of_Sleep,
+    COUNT(student_id) AS total_students,
+    ROUND(AVG(exam_score),2) AS exam_score
+FROM
+    exams_predictions
+GROUP BY
+    Levels_Of_Sleep
+
+ORDER BY
+    exam_score
+
+/*
+What's the gender distribution in each course?
+*/
+SELECT 
+    gender,
+    course,
+    COUNT(student_id)
+FROM
+    exams_predictions
+GROUP BY 
+    gender,
+    course
+ORDER BY
+    gender,
+    course
+
+/*
+How many students have low facility ratings AND less than 6 hours of sleep?
+*/
+SELECT
+    COUNT(student_id) AS lowSleep_and_low_facility
+FROM
+    exams_predictions
+WHERE 
+    facility_rating = 'low' AND sleep_hours <= 6
+
