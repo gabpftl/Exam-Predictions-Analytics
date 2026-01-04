00069161287a21451aab977b576274465a15e264
gabzprogcompany <gabrielquiambao2929@gmail.com>
2026-01-04 09:10:29 +0800

    small changes

--- exam_score_predict.session.sql
+++ exam_score_predict.session.sql
@@ -2,7 +2,7 @@
 What specific question am I trying to answer?
 - Ano-anong factors ang nakakapag bigay ng good grades and bad grades
 - Ano ang pinaka effective or mas na aabsorve ng student sa method ng pag tuturo
-- Correlation ba ang high grades sa gender and ano ang edad ng isang student 
+- May orrelation ba ang gender at edad sa result ng grades
 
 Who will use this analysis?
 - Ang gagamit ng rults nito ay ang mga principal or kung sino man 
@@ -18,7 +18,7 @@ and not effective methods of teaching
 SELECT *
 FROM
     exams_predictions
-LIMIT 5
+LIMIT 50
 
 /*
 How many students are in each course?
@@ -131,4 +131,133 @@ FROM
     exams_predictions
 WHERE 
     facility_rating = 'low' AND sleep_hours <= 6
+
+--------------------------------------------------------------------------
+
+SELECT *
+FROM
+    exams_predictions
+    
+/*
+There are 20,000 students total in the data 
+*/
+SELECT
+    COUNT(student_id) as total_students
+FROM 
+    exams_predictions
+
+/*
+There are 6,579 female students 
+There are 6,726 who did not specify their gender
+There are 6,695 male students 
+*/
+SELECT
+    gender,
+    count(student_id) as total_students
+FROM
+    exams_predictions
+GROUP BY
+    gender
+
+/*
+ba 2,896 female = 983 , male = 954 , other = 959
+bba 2,836 female = 891, male = 969 , other = 976
+b.com 2,864 female = 974 , male = 978 , other = 912
+bca 2,902 female = 928 , male = 969 , other = 1005
+b.tech 2,798 female = 975 , male = 897 , other 926
+b.sc 2,878 female = 898, male = 1012 , other = 968
+diploma 2,826 female = 930 , male = 916 , other = 980
+*/
+SELECT
+    gender,
+    course,
+    count(student_id) as total_students
+FROM
+    exams_predictions
+GROUP BY
+    course,
+    gender
+ORDER BY
+    gender,
+    course
+
+/*
+STUDY METHOD
+1. The most effective of teaching that has biggerst return is COACHING,
+averaging 68.55% percent 
+
+2. Mixed 64.24%
+
+3. Group study 60.97%
+
+4. Online Videos 60.15%
+
+5. Self-Study 58.73%
+*/
+SELECT 
+    study_method,
+    ROUND(AVG(exam_score),2) AS avg_scores
+FROM
+    exams_predictions
+GROUP BY
+    study_method
+
+
+/*
+Exam Difficulty is NOT a key factor for grades 
+*/
+SELECT
+    exam_difficulty, 
+    ROUND(AVG(exam_score),2) as avg_score
+FROM 
+    exams_predictions
+GROUP BY 
+    exam_difficulty
+
+/*
+Gender is NOT a KEY factor
+*/
+SELECT 
+    gender,
+    ROUND(AVG(exam_score)) as avg_score
+FROM
+exams_predictions
+GROUP BY 
+    gender
+
+
+SELECT 
+    internet_access,
+    COUNT(internet_access)
+FROM
+exams_predictions
+GROUP BY 
+    internet_access 
+
+/*
+Definitely the Facilty rating is one of the KEY factors of better results in students
+*/
+SELECT 
+    count(student_id) as total_students,
+    facility_rating,
+    ROUND(AVG(exam_score),2)
+FROM 
+    exams_predictions
+GROUP BY 
+    facility_rating
+
+
+SELECT
+    CASE 
+        WHEN study_hours > 4 THEN 'Low Study Hours' 
+        WHEN study_hours BETWEEN 4 AND 7 THEN 'Medium Study Hours'
+        ELSE 'Long Study Hours'
+    END AS study_hourss,
+    ROUND(AVG(exam_score),2) AS avg_score,
+    MODE(facility_rating),
+    MODE(study_method)
+FROM
+    exams_predictions
+WITHIN GROUP
+    study_hourss
 
